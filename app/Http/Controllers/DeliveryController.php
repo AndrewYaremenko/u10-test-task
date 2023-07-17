635f460357c136bd471bb9ebc44c4d355b5255d0
Andrew Yaremenko <esdesuagk@gmail.com>
2023-07-17 17:15:59 +0300

    change services path and add README.md

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
-use App\Http\Services\DeliveryService;
+use App\Http\Services\Delivery\DeliveryService;
 
 /**
  * DeliveryController
